8f9691909e047e077310efcca7b4856fc7b16995
Enis Nefzi Sanchez <Enis.Sanchez@asos.com>
2019-03-26 13:24:21 +0000

    bubble short

---
+++ Algorithms/BubbleShort.cs
@@ -0,0 +1,28 @@
+﻿namespace Algorithms
+{
+    public class BubbleShort
+    {
+        public static int[] Short(int[] array)
+        {
+            int swaps;
+
+            do
+            {
+                swaps = 0;
+                for (int i = 0; i <= array.Length; i++)
+                {
+                    if (i + 1 <= array.Length -1 && array[i] > array[i + 1])
+                    {
+                        int current = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = current;
+                        swaps = swaps + 1;
+                    }
+                }
+            }
+            while (swaps > 0);  
+
+            return array;
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] {7, 4, 5, 2, 3, 9, 11, 6};
+            var shorted = BubbleShort.Short(array);
+        }
+    }
+}
