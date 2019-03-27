66caa8d250f350867431de6f5b31003d5e32cacb
MrVoid <enis.nefzi.sanchez@gmail.com>
2019-03-27 19:39:05 +0100

    stack

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,13 +1,9 @@
-﻿using System;
-
-namespace Algorithms
+﻿namespace Algorithms
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var array = new int[] {7, 4, 5, 2, 3, 9, 11, 6};
-            var shorted = BubbleShort.Short(array);
         }
     }
 }
---
+++ Algorithms/Stack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public class Stack
+    {
+        private class Node
+        {
+            public int Value { get; set; }
+            public Node Next { get; set; }
+        }
+
+        private Node _head;
+
+        public int Peek()
+        {
+            if (_head == null)
+            {
+                throw new InvalidOperationException();
+            }
+            return _head.Value;
+        }
+
+        public int Pop()
+        {
+            if (_head == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int value = _head.Value;
+            _head = _head.Next;
+            return value;
+        }
+
+        public void Push(int value)
+        {
+            var node = new Node { Value = value };
+            node.Next = _head;
+            _head = node;
+        }
+    }
+}
